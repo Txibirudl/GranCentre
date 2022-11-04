42a32dfa517979a233cde415a4cea3f2d84ebf4b
Txibirudl <iker.domenechl@gmail.com>
2022-11-04 10:31:18 +0100

    Test 2.2

---
+++ app/src/main/java/Hotels/Test.java
@@ -0,0 +1,8 @@
+package Hotels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test {
+}
--- app/src/main/java/com/example/grancentre/MainActivity.java
+++ app/src/main/java/com/example/grancentre/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.grancentre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
